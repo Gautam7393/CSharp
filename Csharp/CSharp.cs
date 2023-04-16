0f029f32bc1a58a7e81ca20383c7d1adc9072c3f
Gautam7393 <gautamchandra7393@gmail.com>
2023-04-16 14:26:07 +0530

    Add project files.

---
+++ Csharp/CSharp.cs
@@ -0,0 +1,15 @@
+﻿using Csharp.Cshapprogram;
+
+namespace MyApp
+{
+    class CSharp
+    {
+
+        public static void Main(string[]args)
+        {
+            BacicCsharpProgram.FindMax();
+            BacicCsharpProgram.FindOccurance();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp/Cshapprogram/BacicCsharpProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.Cshapprogram
+{
+    public class BacicCsharpProgram
+    {
+        public static void FindMax()
+        {
+            Console.WriteLine("Enter the first number : ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            int n2 = int.Parse(Console.ReadLine());
+            if (n1 > n2)
+            {
+                Console.WriteLine("Max is :" + n1);
+            }
+            else
+            {
+                Console.WriteLine("Max is :" + n2);
+            }
+        }
+    }
+
+    
+}
+
+// code done
+
+
+
